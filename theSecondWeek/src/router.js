e09ff11531b02c451e748736e2fef9c5eb9e6f1f
siege lion <fullstackdeveloper@siegedeMBP.lan>
2018-12-25 22:28:37 +0800

    实现路由

--- theSecondWeek/src/router.js
+++ theSecondWeek/src/router.js
@@ -13,10 +13,52 @@ export default new Router({
     {
       path: '/company',
       name: 'company',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import( /* webpackChunkName: "about" */ './views/Company.vue')
+    },
+    {
+      path: '/property',
+      name: 'property',
+      component: () => import('./views/Property.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('./views/Contact.vue')
+    },
+    {
+      path: '/crew',
+      name: 'crew',
+      component: () => import('./views/Crew.vue')
+    },
+    {
+      path: '/culture',
+      name: 'culture',
+      component: () => import('./views/Culture.vue')
+    },
+    {
+      path: '/logistics',
+      name: 'logistics',
+      component: () => import('./views/Logistice.vue')
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: () => import('./views/News.vue')
+    },
+    {
+      path: '/recruitment',
+      name: 'recruitment',
+      component: () => import('./views/Recruitment.vue')
+    },
+    {
+      path: '/security',
+      name: 'security',
+      component: () => import('./views/Security.vue')
+    },
+    {
+      path: '/new',
+      name: 'new',
+      component: () => import('./views/New.vue')
     }
   ]
 })
