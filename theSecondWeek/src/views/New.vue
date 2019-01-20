5eda5ecef219fd0555547768e5f7a32ce22ee9bb
gechen <gechen@world.com>
2019-01-20 22:05:03 +0800

    完成新闻

--- theSecondWeek/src/views/New.vue
+++ theSecondWeek/src/views/New.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="new">
     <content-nav :navName=navName></content-nav>
-    <p class="title">香远北京公司组织举办投资管理操作实践培训</p>
-    <p>来源：时间：2012年12月23日</p>
+    <p class='title'>香远北京公司组织举办投资管理操作实践培训</p>
+    <p class='info'>来源：时间：2012年12月23日</p>
     <div class="new-content">
       <p>香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训
         香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训
@@ -12,7 +12,9 @@
         香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训
         香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训香远北京公司组织举办投资管理操作实践培训
       </p>
-      <div class="new-img"></div>
+      <div class="new-img">
+        <span></span>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +27,49 @@ export default {
   },
 }
 </script>
-<style scoped>
+<style lang='scss' scoped>
+.new {
+  width: 980px;
+  position: relative;
+  display: inline-block;
+  background: #fff;
+  top: 10px;
+  text-align: center;
+  .title {
+    width: 960px;
+    position: relative;
+    left: 10px;
+    top: 20px;
+    font-size: 18px;
+  }
+  .info {
+    font-size: 14px;
+    margin-bottom: 10px;
+  }
+  .new-content {
+    width: 960px;
+    position: relative;
+    left: 10px;
+    border-top: 1px dotted #ccc;
+    p {
+      margin-top: 10px;
+      text-indent: 2em;
+      text-align: left;
+    }
+  }
+  .new-img {
+    width: 960px;
+    position: relative;
+    height: 300px;
+    span {
+      display: inline-block;
+      width: 500px;
+      height: 280px;
+      background: url('../assets/img/06.jpg');
+      background-size: cover;
+    }
+  }
+}
 </style>
 
 
